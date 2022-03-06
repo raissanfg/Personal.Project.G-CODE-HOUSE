9e400994a8770c8bdd80159b8a1ef3944f62cc8a
raissanfg <99144281+raissanfg@users.noreply.github.com>
2022-03-06 12:21:47 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 function toggleDarkMode() {
-  let bodyElement = document.getElementById("main-background"); // target the body element
-  let darkModeButtonElement = document.getElementById("dark-mode"); //target the dark mode button
+    let bodyElement = document.getElementById("main-background");
+    let darkModeButtonElement = document.getElementById("dark-mode-btn");
+    console.log('backgroundImage', bodyElement.style.background)
+    // if the background is black, change background to tan
+    if ( bodyElement.style.background  == "white") {
+      console.log('hi')
+        bodyElement.style.background = "linear-gradient( to top, rgb(123, 151, 123) , rgb(20, 24, 23))";
+      
+     
+        darkModeButtonElement.innerHTML = "Light Mode!";
 
-   // if background is black, change background to white
-  if (bodyElement.style.background === "black") {  // triple equals for strict typing
-   (bodyElement.style.background = "#FFFFFF");
+    }
+   
+    // If background is tan, change background to black 
 
-  }
+    else {
+        console.log('goodbye')
+        bodyElement.style.background = "white"
+        darkModeButtonElement.innerHTML = "Dark Mode!";
 
- // if background is not white, change background to black
-
-else {
-  console.log ("hi");
-     bodyElement.style.background = "black"; // set background to black
-  }
-
+    }
 }
